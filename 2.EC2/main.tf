cdfd2a5a83b5866f6bacc4dbf0b72223d865e4f7
utsavtyagi <utsav.tyagi@corecard.com>
2024-06-13 19:40:22 +0530

    updated

--- 2.EC2/main.tf
+++ 2.EC2/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "example" {
   ami           = "ami-0b20a6f09484773af" # Specify an appropriate AMI ID
   instance_type = "t2.micro"
   key_name = "demo_config_key_us-west-2"
-
+  vpc_security_group_ids = [ "sg-00f1906a3bb9a9383" ]
   tags = {
     Name = "ExampleInstance"
   }
