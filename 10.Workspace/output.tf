c6ef6193972886e2a795f3fd0eb230b7a91d5740
utsavtyagi <utsav.tyagi@corecard.com>
2024-06-18 20:01:47 +0530

    10.Workspace

---
+++ 10.Workspace/output.tf
@@ -0,0 +1,9 @@
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
